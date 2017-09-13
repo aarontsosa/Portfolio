b3f1415b498be462ebfda114ae80e4b990a391fe
Aaron <aarontsosa@outlook.com>
2017-09-13 10:16:50 -0400

    getting flickity to work finally

--- js/custom.js
+++ js/custom.js
@@ -68,26 +68,54 @@ myform.submit(function(event){
 $('#myTabs a').click(function (e) {
 	e.preventDefault()
 	$(this).tab('show')
-  })
+	if(this.text ==="Travel"){
+		console.log('hello')
+}
+		
+})
 
-  var $carousel = $('.carousel').flickity({
-	imagesLoaded: true,
-	percentPosition: false,
-  });
-  
-  var $imgs = $carousel.find('.carousel-cell img');
-  // get transform property
-  var docStyle = document.documentElement.style;
-  var transformProp = typeof docStyle.transform == 'string' ?
-	'transform' : 'WebkitTransform';
-  // get Flickity instance
-  var flkty = $carousel.data('flickity');
-  
-  $carousel.on( 'scroll.flickity', function() {
-	flkty.slides.forEach( function( slide, i ) {
-	  var img = $imgs[i];
-	  var x = ( slide.target + flkty.x ) * -1/3;
-	  img.style[ transformProp ] = 'translateX(' + x  + 'px)';
-	});
-  });
-  
+
+// var $carousel = $('.carousel').flickity({
+// imagesLoaded: true,
+// percentPosition: false
+// });
+
+// var $imgs = $carousel.find('.carousel-cell img');
+// // get transform property
+// var docStyle = document.documentElement.style;
+// var transformProp = typeof docStyle.transform == 'string' ?
+// 'transform' : 'WebkitTransform';
+// // get Flickity instance
+// var flkty = $carousel.data('flickity');
+
+// $carousel.on( 'scroll.flickity', function() {
+// flkty.slides.forEach( function( slide, i ) {
+// 	var img = $imgs[i];
+// 	var x = ( slide.target + flkty.x ) * -1/3;
+// 	img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+// });
+// });
+$('#Travel-tab').on( 'shown.bs.tab', function( event ) {
+	var $carousel = $('.carousel').flickity({
+		imagesLoaded: true,
+		percentPosition: false
+		});
+		
+		var $imgs = $carousel.find('.carousel-cell img');
+		// get transform property
+		var docStyle = document.documentElement.style;
+		var transformProp = typeof docStyle.transform == 'string' ?
+		'transform' : 'WebkitTransform';
+		// get Flickity instance
+		var flkty = $carousel.data('flickity');
+		
+		$carousel.on( 'scroll.flickity', function() {
+		flkty.slides.forEach( function( slide, i ) {
+			var img = $imgs[i];
+			var x = ( slide.target + flkty.x ) * -1/3;
+			img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+		});
+		});
+	// $('[data-target="galery"]').flickity('resize');
+});
+
